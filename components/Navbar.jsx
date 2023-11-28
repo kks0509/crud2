dc861f1e1579ddb17aaf84596b51b666399e0c46
kks0509 <ks509koo@gmail.com>
2023-11-28 12:43:58 +0900

    commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="bg-red-900 flex justify-between items-center px-8 py-4">
+      <Link href="/" className="text-white text-lg font-bold">
+        MongoDB CRUD
+      </Link>
+      <div>MidMenu</div>
+      <Link
+        href="/addTopic"
+        className="bg-yellow-200 text-lg font-bold px-4 py-2 rounded-md text-black"
+      >
+        Add Topic
+      </Link>
+    </nav>
+  )
+}
